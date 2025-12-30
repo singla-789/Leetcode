20b3b127f3915ca7ad487192922f56494e7f7c0f
singla-789 <146032220+singla-789@users.noreply.github.com>
2025-12-30 14:17:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (51.99%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,23 +1,17 @@
 class Solution {
-    HashMap<Integer,Integer> map = new HashMap<>();
 
     public int rob(int[] nums) {
-        return util(nums, 0);
-    }
-
-    int util(int[] nums, int i) {
         int n = nums.length;
-        if (i >= n) return 0;
 
-        if (map.containsKey(i)) return map.get(i);
+        int dp[] = new int[n+1];
 
-        
-        int p1 = nums[i] + util(nums, i + 2);
-       
-        int p2 = util(nums, i + 1);
+        dp[0] = 0;
+        dp[1] = nums[0];
 
-        int profit = Math.max(p1, p2);
-        map.put(i, profit);
-        return profit;
+        for(int i=1;i<n;i++){
+            dp[i+1] = Math.max(dp[i],nums[i]+dp[i-1]);
+        }
+
+        return dp[n];
     }
 }
