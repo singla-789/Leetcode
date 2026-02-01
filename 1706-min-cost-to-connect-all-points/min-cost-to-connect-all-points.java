e393baa101e74dd56c06d4b80012cfcd1550612e
singla-789 <146032220+singla-789@users.noreply.github.com>
2026-02-01 16:58:07 +0530

    Time: 1181 ms (5.99%) | Memory: 129.4 MB (22.57%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,84 +1,39 @@
-import java.util.*;
-
-class DSU {
-    int[] par;
-    int[] rank;
-
-    DSU(int n) {
-        par = new int[n];
-        rank = new int[n];
-        for (int i = 0; i < n; i++)
-            par[i] = i;
-    }
-
-    public int find(int a) {
-        if (par[a] != a)
-            par[a] = find(par[a]);
-        return par[a];   // ✅ FIX
-    }
-
-    public boolean union(int a, int b) {
-        int pa = find(a);
-        int pb = find(b);
-
-        if (pa == pb) return false;
-
-        if (rank[pa] == rank[pb]) {
-            par[pb] = pa;
-            rank[pa]++;
-        } else if (rank[pa] < rank[pb]) {
-            par[pa] = pb;
-        } else {
-            par[pb] = pa;
-        }
-        return true;
-    }
-}
-
-class Edge {
-    int u, v, w;
-
-    Edge(int u, int v, int w) {
-        this.u = u;
-        this.v = v;
+class pair{
+    int i ;
+    int w;
+    pair(int i,int w){
+        this.i = i;
         this.w = w;
     }
 }
 
 class Solution {
-
-    public int cost(int[] p1, int[] p2) {
-        return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
+    public int cost(int[] a,int[] b){
+        return Math.abs(a[0]-b[0]) + Math.abs(a[1]-b[1]);
     }
 
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
-        DSU dsu = new DSU(n);
+        PriorityQueue<pair> pq = new PriorityQueue<>((a,b) -> a.w - b.w);
+        boolean[] vis = new boolean[n];
 
-        ArrayList<Edge> edges = new ArrayList<>();
+        int ans =0;
 
-        // Build all edges
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                edges.add(new Edge(i, j, cost(points[i], points[j])));
+        pq.add(new pair(0,0));
+        while(!pq.isEmpty()){
+            pair p = pq.poll();
+
+            if(!vis[p.i]){
+                vis[p.i] = true;
+                ans += p.w;
+                for(int j =0;j<n;j++){
+                    int c = cost(points[p.i],points[j]);
+                    
+                    pq.add(new pair(j,c));
+                }
             }
-        }
+        } 
 
-        // Sort by weight
-        Collections.sort(edges, (a, b) -> a.w - b.w);
-
-        int ans = 0;
-        int edgesUsed = 0;
-
-        // Kruskal
-        for (Edge e : edges) {
-            if (dsu.union(e.u, e.v)) {
-                ans += e.w;
-                edgesUsed++;
-                if (edgesUsed == n - 1) break;
-            }
-        }
-
-        return ans;
+        return ans;   
     }
 }
