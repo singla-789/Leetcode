174d7975dec580ddaeba61033f476dfbade41735
singla-789 <146032220+singla-789@users.noreply.github.com>
2026-02-04 12:53:51 +0530

    Time: 29 ms (61.32%) | Memory: 81.3 MB (50.00%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,29 +1,30 @@
 class Solution {
-    private boolean solve(int n, int[] quantities, int item) {
-        if (item == 0) return false;
-        int store = 0;
-        for (int product : quantities) {
-            store += (product - 1) / item + 1;
-            if (store > n) return false;
-        }
-        return true;
-    }
+    public int minimizedMaximum(int n, int[] q) {
 
-    public int minimizedMaximum(int n, int[] quantities) {
-        int low = 1;
-        int high = Arrays.stream(quantities).max().getAsInt();
+        int l = 1;
+        int r =Arrays.stream(q).max().getAsInt();
         int ans = -1;
         
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (solve(n, quantities, mid)) {
+        while(l<=r){
+            int mid = l +(r-l)/2;
+            if(can(n,mid,q)){
+                r = mid -1;
                 ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            }else{
+                l = mid +1;
             }
         }
-        
         return ans;
+    }
+
+    public boolean can(int n,int k,int[] q){
+        if(k==0) return false;
+        int s = 0;
+        for(int i: q){
+            s += (i-1)/k+1;
+            if(s>n) return false;
+        }
+        return true;
+
     }
 }
