0eb70cef465a4dce05843bb1f1fcd9d27d4807bb
singla-789 <146032220+singla-789@users.noreply.github.com>
2026-02-14 15:27:30 +0530

    Time: 5 ms (19.43%) | Memory: 48.6 MB (21.20%) - LeetSync

--- 815-champagne-tower/champagne-tower.java
+++ 815-champagne-tower/champagne-tower.java
@@ -1,19 +1,14 @@
 class Solution {
     public double champagneTower(int pour, int i, int j) {
         double dp[][] = new double[i+1][i+1];
-
         dp[0][0] = pour;
-
         for(int a=1;a<i+1;a++){
             for(int b = 0;b<=a;b++){
                 double l = b-1>=0 ?Math.max(0,dp[a-1][b-1]-1)/2.0:0;
                 double r= b<=i-1?Math.max(0,dp[a-1][b]-1)/2.0:0;
-
                 dp[a][b] = l+r;
             }
         }
-
         return Math.min(1,dp[i][j]);
-
     }
 }
