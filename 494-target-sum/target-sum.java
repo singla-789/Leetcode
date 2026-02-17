708882873e850834bda1e429288746e1ae10940d
singla-789 <146032220+singla-789@users.noreply.github.com>
2026-02-17 15:29:03 +0530

    Time: 7 ms (65.52%) | Memory: 45.3 MB (37.11%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,22 +1,26 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
         int sum = 0;
         for(int num : nums) sum += num;
 
         if (sum < Math.abs(target)) return 0;
+
         if ((target + sum) % 2 != 0) return 0;
 
         int t = (target + sum) / 2;
-
-        int[] dp = new int[t + 1];
-        dp[0] = 1;
-
-        for(int num : nums) {
-            for(int j = t; j >= num; j--) {
-                dp[j] += dp[j - num];
+        int dp[][] = new int[n+1][t+1];
+        dp[0][0] =1;
+        for(int i =1;i<n+1;i++){
+            for(int j =0;j<t+1;j++){
+                dp[i][j] = dp[i-1][j];
+                if(j>=nums[i-1]){
+                    dp[i][j] = dp[i][j] + dp[i-1][j-nums[i-1]];
+                }
             }
         }
 
-        return dp[t];
+        return dp[n][t];
+
     }
 }
