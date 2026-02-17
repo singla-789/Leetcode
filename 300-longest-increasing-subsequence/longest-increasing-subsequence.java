e02399eaca9dbd4b32caef706c3e23966f57c3e6
singla-789 <146032220+singla-789@users.noreply.github.com>
2026-02-17 16:29:42 +0530

    Time: 37 ms (74.79%) | Memory: 46.1 MB (70.27%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,30 +1,18 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        ArrayList<Integer> ans = new ArrayList<>();
-        ans.add(nums[0]);
-        for(int i=1;i<n;i++){
-            if(nums[i]>ans.get(ans.size()-1)){
-                ans.add(nums[i]);
-            }else if(nums[i]<ans.get(ans.size()-1)){
-                ans.set(bs(ans,nums[i]),nums[i]);
+        int dp[] = new int[n];
+        Arrays.fill(dp,1);
+        int ans= 0;
+        for(int i =0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i] = Math.max(dp[j]+1,dp[i]);
+                }
             }
+            ans = Math.max(ans,dp[i]);
         }
-        return ans.size();
-    }
 
-    public int bs(ArrayList<Integer> ans,int a){
-        int l =0;
-        int r = ans.size()-1;
-        while(l<=r){
-            int m = l +(r-l)/2;
-            if(ans.get(m) == a) return m;
-            if(ans.get(m)>a){
-                r = m-1;
-            }else{
-                l = m+1;
-            }
-        }
-        return l;
+        return ans;
     }
 }
