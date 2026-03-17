6e8895047cac79645a3d96acc029ca9bbb7cb127
singla-789 <146032220+singla-789@users.noreply.github.com>
2026-03-17 14:52:57 +0530

    Time: 13 ms (86.96%) | Memory: 114.7 MB (37.39%) - LeetSync

---
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int largestSubmatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        // Build heights
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] != 0) {
+                    matrix[i][j] += matrix[i - 1][j];
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int[] row = matrix[i].clone();
+            Arrays.sort(row);
+
+            for (int j = m - 1; j >= 0; j--) {
+                int height = row[j];
+                int width = m - j;
+                ans = Math.max(ans, height * width);
+            }
+        }
+
+        return ans;
+    }
+}
