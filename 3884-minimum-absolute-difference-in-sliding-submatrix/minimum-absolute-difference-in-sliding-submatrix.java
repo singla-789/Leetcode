d72a113c31bba814d4d93dc7c337bffed07085ba
singla-789 <146032220+singla-789@users.noreply.github.com>
2026-03-20 14:33:37 +0530

    Time: 26 ms (48.94%) | Memory: 47.6 MB (68.09%) - LeetSync

--- 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
@@ -2,9 +2,7 @@ class Solution {
     public int[][] minAbsDiff(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-
         int[][] ans = new int[m - k + 1][n - k + 1];
-
         for (int i = 0; i <= m - k; i++) {
             for (int j = 0; j <= n - k; j++) {
                 TreeSet<Integer> set = new TreeSet<>();
@@ -13,11 +11,9 @@ class Solution {
                         set.add(grid[x][y]);
                     }
                 }
-
                 Iterator<Integer> it = set.iterator();
                 int pre = it.next();
                 int min = Integer.MAX_VALUE;
-
                 while(it.hasNext()){
                     int curr = it.next();
                     min = Math.min(min,Math.abs(curr-pre));
