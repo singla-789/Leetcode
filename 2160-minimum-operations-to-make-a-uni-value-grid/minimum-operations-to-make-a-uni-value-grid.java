9a93174ce7ace0b1d851f81c9d4ca37b7f552846
singla-789 <146032220+singla-789@users.noreply.github.com>
2026-04-28 12:18:12 +0530

    Time: 95 ms (55.72%) | Memory: 117.8 MB (9.16%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -2,7 +2,6 @@ class Solution {
     public int minOperations(int[][] grid, int x) {
         ArrayList<Integer> list = new ArrayList<>();
         int r = grid[0][0]%x;
-
         for(int i =0;i<grid.length;i++){
             for(int j =0;j<grid[0].length;j++){
                 list.add(grid[i][j]);
@@ -11,20 +10,13 @@ class Solution {
                 }
             }
         }
-
         Collections.sort(list);
         int n = list.size();
-
-        int t = list.get((n-1)/2);
+        int t = list.get((n)/2);
         int ans =0;
         for(int i : list){
             ans += Math.abs(i-t)/x;
         }
-
         return ans;
-
-
-
-
     }
 }
