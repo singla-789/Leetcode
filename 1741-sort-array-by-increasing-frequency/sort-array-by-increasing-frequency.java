71d9b91cb05f2ceaa2b9c99e836f52d41092af20
singla-789 <146032220+singla-789@users.noreply.github.com>
2026-05-17 11:53:04 +0530

    Time: 8 ms (67.39%) | Memory: 45.7 MB (55.76%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -1,40 +1,38 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
 
-        HashMap<Integer, Integer> freq = new HashMap<>();
-
-        // Count frequency
-        for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        for(int i : nums){
+            map.put(i,map.getOrDefault(i,0)+1);
         }
 
-        // Convert to Integer[] for custom sorting
-        Integer[] arr = new Integer[nums.length];
+        int n = nums.length;
+        Integer arr[] = new Integer[n];
 
-        for (int i = 0; i < nums.length; i++) {
+        for(int i =0;i<n;i++){
             arr[i] = nums[i];
         }
 
-        // Custom sort
-        Arrays.sort(arr, (a, b) -> {
+        Arrays.sort(arr,(a,b) -> {
 
-            int fa = freq.get(a);
-            int fb = freq.get(b);
+            int fa = map.get(a);
+            int fb = map.get(b);
 
-            // Increasing frequency
-            if (fa != fb) {
-                return fa - fb;
-            }
+            if(fa!=fb) return fa-fb;
 
-            // Same frequency -> decreasing value
-            return b - a;
+            return b-a;
         });
 
-        // Convert back to int[]
-        for (int i = 0; i < nums.length; i++) {
+
+        for(int i =0;i<n;i++){
             nums[i] = arr[i];
         }
 
         return nums;
+
+
+
+
+
     }
 }
