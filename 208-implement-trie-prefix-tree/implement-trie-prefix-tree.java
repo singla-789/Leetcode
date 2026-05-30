c1481c1434663dbec02f3d2b07b7448f214eb897
singla-789 <146032220+singla-789@users.noreply.github.com>
2026-05-30 22:47:25 +0530

    Time: 33 ms (64.15%) | Memory: 61.8 MB (82.88%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,53 +1,56 @@
 class Trie {
 
-    class Node {
-        Node[] children;
+    class Node{
+        Node node[];
         boolean end;
 
-        Node() {
-            children = new Node[26];
+        Node(){
+            node = new Node[26];
+            Arrays.fill(node,null);
             end = false;
         }
     }
 
     Node root;
-
     public Trie() {
         root = new Node();
     }
-
+    
     public void insert(String word) {
         Node curr = root;
-
-        for (char c : word.toCharArray()) {
-            if (curr.children[c - 'a'] == null) {
-                curr.children[c - 'a'] = new Node();
+        for(char a : word.toCharArray()){
+            if(curr.node[a-'a'] == null){
+                Node newNode = new Node();
+                curr.node[a-'a'] = newNode;
             }
-            curr = curr.children[c - 'a'];
+            curr = curr.node[a-'a'];
         }
-
         curr.end = true;
     }
-
+    
     public boolean search(String word) {
         Node curr = root;
-
-        for (char c : word.toCharArray()) {
-            if (curr.children[c - 'a'] == null) return false;
-            curr = curr.children[c - 'a'];
+        for(char a : word.toCharArray()){
+            if(curr.node[a-'a'] == null) return false;
+            curr = curr.node[a-'a'];
         }
-
         return curr.end;
     }
-
+    
     public boolean startsWith(String prefix) {
         Node curr = root;
-
-        for (char c : prefix.toCharArray()) {
-            if (curr.children[c - 'a'] == null) return false;
-            curr = curr.children[c - 'a'];
+        for(char a : prefix.toCharArray()){
+            if(curr.node[a-'a'] == null) return false;
+            curr = curr.node[a-'a'];
         }
-
         return true;
     }
 }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
