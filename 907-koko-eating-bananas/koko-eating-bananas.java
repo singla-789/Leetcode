ff7fceaa26d298017a6276a4f5d6adc8742ed401
singla-789 <146032220+singla-789@users.noreply.github.com>
2026-06-14 23:22:36 +0530

    Time: 6 ms (99.49%) | Memory: 48.5 MB (9.34%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length;
+
+        int l =1;
+        int r = Integer.MIN_VALUE;
+        for(int a: piles){
+            r = Math.max(r,a);
+        }
+
+        while(l<r){
+            int m = l +(r-l)/2;
+
+            if(help(m,piles,h)){
+                r = m;
+            }else{
+                l = m+1;
+            }
+        }
+
+        return l;
+
+    }
+
+    public boolean help(int m,int[] arr,int h){
+        int t = 0;
+
+        for(int a : arr){
+            t += (a+m-1)/m;
+        }
+
+        return h>=t;
+    }
+}
