0d4c4691c45712a0afe2e17c9e8c01a0df7ce166
singla-789 <146032220+singla-789@users.noreply.github.com>
2026-06-21 12:00:06 +0530

    Time: 9 ms (81.71%) | Memory: 82.3 MB (96.28%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -1,48 +1,21 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        costs = countSort(costs);
+        int[] freq = new int[100001];
+
+        for (int cost : costs) {
+            freq[cost]++;
+        }
+
         int ans = 0;
-        int n = costs.length;
-        for (int i = 0; i < n; i++) {
-            if (costs[i] <= coins) {
+
+        for (int cost = 1; cost <= 100000; cost++) {
+            while (freq[cost] > 0 && coins >= cost) {
+                coins -= cost;
+                freq[cost]--;
                 ans++;
-                coins -= costs[i];
-            } else {
-                break;
             }
         }
 
         return ans;
-    }
-
-    public int[] countSort(int[] arr) {
-        int n = arr.length;
-        int max = Integer.MIN_VALUE;
-        for (int a : arr) {
-            max = Math.max(a, max);
-        }
-
-        int[] count = new int[max + 1];
-
-        for (int i = 0; i < n; i++) {
-            count[arr[i]]++;
-        }
-
-        for (int i = 1; i <=max; i++) {
-            count[i] += count[i - 1];
-        }
-
-        int[] ans = new int[n];
-
-        for (int i = n - 1; i >= 0; i--) {
-            int a = arr[i];
-            ans[count[a] - 1] = a;
-
-            count[a]--;
-
-        }
-
-        return ans;
-
     }
 }
