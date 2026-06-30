b0e9a7ee3ccd6e08be6e444f1d22c9054d077058
singla-789 <146032220+singla-789@users.noreply.github.com>
2026-06-30 13:14:25 +0530

    Time: 12 ms (84.52%) | Memory: 46.4 MB (45.69%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -4,7 +4,7 @@ class Solution {
         int[] p = {-1, -1, -1};
 
         for (int i = 0; i < s.length(); i++) {
-            p[(s.charAt(i) & 31) - 1] = i;
+            p[s.charAt(i)-'a'] = i;
             res += Math.min(p[0], Math.min(p[1], p[2])) + 1;
         }
 
